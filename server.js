73a2ec7cae7b07b0eb29c47d5fad2075f8efb455
Nadeem Hassan <YOUR_GITHUB_EMAIL>
2026-09-17 23:21:45 -0700

    Fix Vercel deployment

--- server.js
+++ server.js
@@ -136,6 +136,10 @@ app.delete("/api/teachers/:id", async (req, res) => {
 
 const PORT = process.env.PORT || 5500;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
+
+module.exports = app;

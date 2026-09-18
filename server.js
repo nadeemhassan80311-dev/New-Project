59e32903759e45899a8201e85bf3a57d2c4bdaa4
Nadeem Hassan <YOUR_GITHUB_EMAIL>
2026-09-17 23:43:07 -0700

    Fix Express Vercel root route

--- server.js
+++ server.js
@@ -9,7 +9,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("public"));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/index.html");
+});
 
 const supabaseUrl = process.env.SUPABASEURL;
 const supabaseKey = process.env.SUPABASEKEY;
